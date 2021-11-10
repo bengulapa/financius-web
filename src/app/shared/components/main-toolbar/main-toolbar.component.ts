169f569cf33ab71d623149c7a0401da0070db20c
bengulapa <benedickgulapa@gmail.com>
2021-11-10 10:39:02 +0800

    setup dark theme

--- src/app/shared/components/main-toolbar/main-toolbar.component.ts
+++ src/app/shared/components/main-toolbar/main-toolbar.component.ts
@@ -1,12 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-main-toolbar',
   templateUrl: './main-toolbar.component.html',
   styleUrls: ['./main-toolbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainToolbarComponent implements OnInit {
-  constructor() {}
+  get isDarkMode(): boolean {
+    return localStorage.getItem('financius.theme') === 'dark';
+  }
 
-  ngOnInit(): void {}
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    if (this.isDarkMode) {
+      this.setDarkMode();
+    }
+  }
+
+  toggleDarkMode() {
+    if (this.isDarkMode) {
+      this.renderer.removeClass(document.body, 'dark');
+      localStorage.setItem('financius.theme', 'light');
+    } else {
+      this.setDarkMode();
+    }
+  }
+
+  private setDarkMode() {
+    this.renderer.addClass(document.body, 'dark');
+    localStorage.setItem('financius.theme', 'dark');
+  }
 }
