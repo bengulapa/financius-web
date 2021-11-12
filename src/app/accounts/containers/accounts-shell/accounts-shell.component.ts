422d659e9c0db1a8f38c730b55c1b3c83db325b5
bengulapa <benedickgulapa@gmail.com>
2021-11-12 20:50:13 +0800

    update accounts table setting

--- src/app/accounts/containers/accounts-shell/accounts-shell.component.ts
+++ src/app/accounts/containers/accounts-shell/accounts-shell.component.ts
@@ -2,18 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccountsService } from 'src/app/core/services/accounts.service';
 import { Account } from 'src/app/shared/models/entities.models';
+import { ObservableBaseComponent } from 'src/app/shared/observable-base.component';
 
 @Component({
   selector: 'app-accounts-shell',
   templateUrl: './accounts-shell.component.html',
   styleUrls: ['./accounts-shell.component.scss'],
 })
-export class AccountsShellComponent implements OnInit {
-  accounts$!: Observable<Account[]>;
+export class AccountsShellComponent
+  extends ObservableBaseComponent
+  implements OnInit
+{
+  accounts$?: Observable<Account[]>;
 
-  constructor(private service: AccountsService) {}
+  constructor(private service: AccountsService) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.accounts$ = this.service.getAll();
+    this.service.getAll();
+    this.accounts$ = this.service.entities$;
   }
 }
