456f29856328beef24122f3faf320a7ae5d76a74
bengulapa <benedickgulapa@gmail.com>
2021-11-24 14:26:43 +0800

    use accounts facade

--- src/app/dashboard/containers/dashboard-shell/dashboard-shell.component.ts
+++ src/app/dashboard/containers/dashboard-shell/dashboard-shell.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AccountsService } from 'src/app/core/services/accounts.service';
+import { AccountsFacade } from 'src/app/accounts/state/accounts.facade';
 import { TransactionsService } from 'src/app/core/services/transactions.service';
 import { getLocaleMonthName } from 'src/app/core/utilities/date.utils';
 import { Account, Transaction } from 'src/app/shared/models/entities.models';
@@ -11,18 +11,18 @@ import { Account, Transaction } from 'src/app/shared/models/entities.models';
   styleUrls: ['./dashboard-shell.component.scss'],
 })
 export class DashboardShellComponent implements OnInit {
-  accounts$!: Observable<Account[]>;
+  accounts$?: Observable<Account[]>;
   transactions$!: Observable<Transaction[]>;
   expenses$!: Observable<Transaction[]>;
   title!: string;
 
   constructor(
-    private accountsService: AccountsService,
+    public accountsFacade: AccountsFacade,
     private transactionsService: TransactionsService
   ) {}
 
   ngOnInit(): void {
-    this.accounts$ = this.accountsService.getAccounts();
+    this.accountsFacade.retrieve();
 
     const currentDate = new Date(),
       currentMonth = currentDate.getMonth(),
