0e55bd7a2878ebaf5b40a9f5344beab279023537
bengulapa <benedickgulapa@gmail.com>
2021-11-30 16:20:49 +0800

    refactor tag table

--- src/app/tags/components/tags-table/tags-table.component.ts
+++ src/app/tags/components/tags-table/tags-table.component.ts
@@ -1,10 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Tag } from 'src/app/shared/models/entities.models';
 import { TableBaseComponent } from 'src/app/shared/table-base.component';
 
@@ -14,17 +8,6 @@ import { TableBaseComponent } from 'src/app/shared/table-base.component';
   styleUrls: ['./tags-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TagsTableComponent
-  extends TableBaseComponent<Tag>
-  implements OnChanges
-{
+export class TagsTableComponent extends TableBaseComponent<Tag> {
   displayedColumns = ['name', 'actions'];
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.data?.currentValue) {
-      this.dataSource = new MatTableDataSource(this.data || []);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    }
-  }
 }
