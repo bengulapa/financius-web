3a6a704dc2247fce8bdf776f986e754b3957019f
bengulapa <benedickgulapa@gmail.com>
2021-12-06 13:47:56 +0800

    add selectAccountTransactionsById selector instead of setting a selectedAccount

--- src/app/accounts/state/accounts.selectors.ts
+++ src/app/accounts/state/accounts.selectors.ts
@@ -6,40 +6,23 @@ import * as fromReducer from './accounts.reducer';
 
 const { selectAll } = fromReducer.accountsAdapter.getSelectors();
 
-const selectState = createFeatureSelector<fromReducer.AccountsState>(
-  fromReducer.featureKey
-);
+const selectState = createFeatureSelector<fromReducer.AccountsState>(fromReducer.featureKey);
 
 const selectAllAccounts = createSelector(selectState, selectAll);
 
-export const selectAccounts = createSelector(selectAllAccounts, (accounts) =>
-  accounts?.filter((t) => t.modelState === ModelState.Normal)
-);
+export const selectAccounts = createSelector(selectAllAccounts, (accounts) => accounts?.filter((t) => t.modelState === ModelState.Normal));
 
-export const selectAccount = createSelector(
-  selectState,
-  (state) => state.selectedAccount
-);
+export const selectAccount = createSelector(selectState, (state) => state.selectedAccount);
 
-export const selectLoading = createSelector(
-  selectState,
-  (state) => state.loading
-);
+export const selectLoading = createSelector(selectState, (state) => state.loading);
 
-export const selectEntitiesLoaded = createSelector(
-  selectState,
-  (state) => state.entitiesLoaded
-);
+export const selectEntitiesLoaded = createSelector(selectState, (state) => state.entitiesLoaded);
 
-export const selectActiveAccounts = createSelector(
-  selectAllAccounts,
-  (accounts) =>
-    _.orderBy(
-      accounts?.filter(
-        (t) => t.modelState === ModelState.Normal && t.includeInTotals
-      ),
-      'name'
-    )
+export const selectActiveAccounts = createSelector(selectAllAccounts, (accounts) =>
+  _.orderBy(
+    accounts?.filter((t) => t.modelState === ModelState.Normal && t.includeInTotals),
+    'name'
+  )
 );
 
 export const selectAccountTransactions = createSelector(
@@ -47,22 +30,22 @@ export const selectAccountTransactions = createSelector(
   transactionsSelectors.selectConfirmedTransactions,
   (account, transactions) => {
     return account && transactions?.length
-      ? transactions.filter(
-          (t) =>
-            t.accountFrom?.id === account.id || t.accountTo?.id === account.id
-        )
+      ? transactions.filter((t) => t.accountFrom?.id === account.id || t.accountTo?.id === account.id)
       : [];
   }
 );
 
-export const selectAccountsPageViewModel = createSelector(
-  selectAccounts,
-  selectLoading,
-  (accounts, loading) => ({
-    accounts,
-    loading,
-  })
-);
+export const selectAccountTransactionsById = (accountId: string) =>
+  createSelector(transactionsSelectors.selectConfirmedTransactions, (transactions) => {
+    return accountId && transactions?.length
+      ? transactions.filter((t) => t.accountFrom?.id === accountId || t.accountTo?.id === accountId)
+      : [];
+  });
+
+export const selectAccountsPageViewModel = createSelector(selectAccounts, selectLoading, (accounts, loading) => ({
+  accounts,
+  loading,
+}));
 
 export const selectAccountPageViewModel = createSelector(
   selectAccount,
